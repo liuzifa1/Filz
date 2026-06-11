34540fedd1abd1b1938bd7d4fd57c8e14f22c3fe
liuzifa1 <47682514+liuzifa1@users.noreply.github.com>
2026-06-11 18:50:59 +0800

    continue polish

---
+++ liquidsend/Views/SettingsView/AboutAppView.swift
@@ -0,0 +1,15 @@
+//
+//  AboutAppView.swift
+//  liquidsend
+//
+//  Created by liu zifa1 on 1/23/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct AboutAppView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
